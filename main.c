80c5af8067abc17a6c3409bf9a2b1a0c8193c555
NickitaTrofimov <32363673+NickitaTrofimov@users.noreply.github.com>
2017-10-13 15:56:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,59 +2,67 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-float C_F (float x){
+float C_F (float x)
+{
 return x*5.0/9.0+32;
 }
-float F_C (float x){
+float F_C (float x)
+{
 return (x-32)*5.0/9.0;
 }
-float C_K (float x){
+float C_K (float x)
+{
 return x+273.15;
 }
-float K_C (float x){
+float K_C (float x)
+{
 return x-273.15;
 }
 
 int main(int argc, char *argv[]){
 float temp = atof(argv[1]);
-if(argc==3){
-switch (*argv[2]){
-case 'C':
-if (temp < -273.15)
-printf("Temperature < absolute zero for Celsius!\n");
-else
-printf("%.2f F\n%.2f K", C_F(temp), C_K(temp));
-break;
-case 'F':
-if (temp < -459.67)
-printf("Temperature < absolute zero for Fahrenheit!\n");
-else
-printf("%.2f C\n%.2f K", F_C(temp), C_K(F_C(temp)));
-break;
-case 'K':
-if (temp < 0)
-printf("Temperature < absolute zero for Kelvin!\n");
-else
-printf("%.2f C\n%.2f F", K_C(temp), C_F(K_C(temp)));
-break;
+printf("\n");
+if(argc==3)
+{
+    switch (*argv[2])
+    {
+        case 'C':
+            if (temp < -273.15)
+                printf("Temperature < absolute zero for Celsius!\n");
+            else
+                printf("%.2f F\n%.2f K", C_F(temp), C_K(temp));
+                break;
+        case 'F':
+            if (temp < -459.67)
+                printf("Temperature < absolute zero for Fahrenheit!\n");
+            else
+                printf("%.2f C\n%.2f K", F_C(temp), C_K(F_C(temp)));
+                break;
+        case 'K':
+            if (temp < 0)
+                printf("Temperature < absolute zero for Kelvin!\n");
+            else
+                printf("%.2f C\n%.2f F", K_C(temp), C_F(K_C(temp)));
+                break;
+    }
 }
-  
-else{
-printf (temp, "%.2f C:\n");
-if (temp < -273.15)
-printf("Temperature < absolute zero for Celsius!\n");
 else
-printf("%.2f F\n%.2f K", C_F(temp), C_K(temp));
-printf (temp, "%.2f F:\n");
-if (temp < -459.67)
-printf("Temperature < absolute zero for Fahrenheit!\n");
-else
-printf("%.2f C\n%.2f K", F_C(temp), C_K(F_C(temp)));
-printf (temp, "%.2f K:\n");
-if (temp < 0)
-printf("Temperature < absolute zero for Kelvin!\n");
-else
-printf("%.2f C\n%.2f F", K_C(temp), C_F(K_C(temp)));
+{
+    printf ("%.2f", temp, "C:\n");
+    if (temp < -273.15)
+        printf("Temperature < absolute zero for Celsius!\n");
+    else
+        printf("%.2f F\n%.2f K", C_F(temp), C_K(temp), "\n");
+    printf ("%.2f", temp, "F:\n");
+    if (temp < -459.67)
+        printf("Temperature < absolute zero for Fahrenheit!\n");
+    else
+        printf("%.2f C\n%.2f K", F_C(temp), C_K(F_C(temp)), "\n");
+    printf ("%.2f", temp, "K:\n");
+    if (temp < 0)
+        printf("Temperature < absolute zero for Kelvin!\n");
+    else
+        printf("%.2f C\n%.2f F", K_C(temp), C_F(K_C(temp)), "\n");
 }
 return 0;
 }
