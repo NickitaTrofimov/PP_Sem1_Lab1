2aee31bb255b5a929acd531c43632b9ed4b65175
NickitaTrofimov <32363673+NickitaTrofimov@users.noreply.github.com>
2017-10-17 00:18:07 +0400

    Update main.c

--- main.c
+++ main.c
@@ -28,51 +28,69 @@ if(argc==3)
     {
         case 'C':
             if (temp < -273.15)
+            {
                 printf("Temperature < absolute zero for Celsius!\n");
-            else{
+            }
+            else
+            {
                 printf("%.2f F\n", C_F(temp));
                 printf("%.2f K\n", C_K(temp));
-                break;
             }
+                break;
         case 'F':
             if (temp < -459.67)
+            {
                 printf("Temperature < absolute zero for Fahrenheit!\n");
-            else{
+            }
+            else
+            {
                 printf("%.2f C\n", F_C(temp));
                 printf("%.2f K\n", C_K(F_C(temp)));
-                break;
             }
+                break;
         case 'K':
             if (temp < 0)
+            {
                 printf("Temperature < absolute zero for Kelvin!\n");
-            else{
+            }
+            else
+            {
                 printf("%.2f C\n", K_C(temp));
                 printf("%.2f F", C_F(K_C(temp)));
-                break;
             }
+                break;
     }
 }
-else
 
+else
 {
     printf("%.2f C:\n", temp);
     if (temp < -273.15)
+    {
         printf("Temperature < absolute zero for Celsius!\n");
-    else{
+    }
+    else
+    {
         printf("%.2f F\n", C_F(temp));
         printf("%.2f K\n", C_K(temp));
     }
     printf("%.2f F:\n", temp);
     if (temp < -459.67)
+    {
         printf("Temperature < absolute zero for Fahrenheit!\n");
-    else{
+    }
+    else
+    {
         printf("%.2f C\n", F_C(temp));
         printf("%.2f K\n", C_K(F_C(temp)));
     }
     printf ("%.2f K:\n", temp);
     if (temp < 0)
+    {
         printf("Temperature < absolute zero for Kelvin!\n");
-    else{
+    }
+    else
+    {
         printf("%.2f C\n", K_C(temp));
         printf("%.2f F", C_F(K_C(temp)));
     }
